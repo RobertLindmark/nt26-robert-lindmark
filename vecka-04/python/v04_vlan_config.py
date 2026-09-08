1ac65c4a025c6699c9b4c5d2606257de0c90b7f3
RobertLindmark <robert.lindmark@hotmail.com>
2026-09-08 09:41:17 +0200

    Have done VLAN pyton script

--- vecka-04/python/v04_vlan_config.py
+++ vecka-04/python/v04_vlan_config.py
@@ -1,1 +1,18 @@
 
+def vlan_config(number, name):
+    rader = []
+    rader.append(f"vlan {number}")
+    rader.append(f" name {name}")
+    return rader
+
+#Nat på Nordvik. byt ut mot de du ska använda. (Configar Nordvik så den liknar)
+vlans = {
+    10: "KONTOR",
+    20: "EKONOMI",
+    30: "GAST",
+    40: "DRIFT",
+    }
+
+for number in vlans:
+    for rad in vlan_config(number, vlans[number]):
+        print(rad)
